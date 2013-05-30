99bc48dd7a748c77fc56e6ad978de1ef3414967c
Dedos <raulrmh24@gmail.com>
2013-05-30 14:05:13 -0600

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamApp::Application.config.secret_token = '575ddee6fa7086befd06a63c1ed9eed09ba64bbb46ac7be64615719dd5420358b44fb846b55a6206763a8ed9bf7505b449aa41a8c7caf2bc3f93f3ea5c8aa0ae'
